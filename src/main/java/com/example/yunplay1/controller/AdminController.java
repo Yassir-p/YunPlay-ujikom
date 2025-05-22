1516c94929afd3528c2a4a788ef91bdc2ce5bb70
yassir <myassiralhakim@gmail.com>
2025-05-22 08:04:44 +0700

    add dashboard admin

---
+++ src/main/java/com/example/yunplay1/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.example.yunplay1.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminController {
+    @FXML
+    private Button btnAddData;
+
+    @FXML
+    private Button btnShowDetails;
+
+    @FXML
+    private void onBtnAddClick() {
+
+    }
+
+    @FXML
+    private void onBtnShowDataClick() {
+
+    }
+}
