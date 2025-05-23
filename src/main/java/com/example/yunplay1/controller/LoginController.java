690e232e56f73ebff6fa24110c3bce934782d940
yassir <myassiralhakim@gmail.com>
2025-05-23 09:22:36 +0700

    add show details & session

--- src/main/java/com/example/yunplay1/controller/LoginController.java
+++ src/main/java/com/example/yunplay1/controller/LoginController.java
@@ -41,7 +41,7 @@ public class LoginController {
 
         try {
             Connection conn = Koneksi.getKonek();
-            String query = "SELECT * FROM users WHERE username=? AND password=?";
+            String query = "SELECT role FROM users WHERE username=? AND password=?";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, username);
             stmt.setString(2, password);
@@ -82,9 +82,9 @@ public class LoginController {
         try {
             RegisterView registerView = new RegisterView();
             Stage registerStage = new Stage();
+            registerView.start(registerStage);
             Stage currentPage = (Stage) btnSignUp.getScene().getWindow();
             currentPage.close();
-            registerView.start(registerStage);
         } catch (Exception e) {
             e.printStackTrace();
         }
