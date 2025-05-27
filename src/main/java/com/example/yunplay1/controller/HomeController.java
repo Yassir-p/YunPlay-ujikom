9c4fb111b6e9e642e4aefaadc5910561088eca48
yassir <myassiralhakim@gmail.com>
2025-05-27 08:38:11 +0700

    update

--- src/main/java/com/example/yunplay1/controller/HomeController.java
+++ src/main/java/com/example/yunplay1/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.example.yunplay1.controller;
 
+import com.example.yunplay1.views.LoginView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import static com.example.yunplay1.Session.clearSession;
 
 public class HomeController {
     @FXML
@@ -9,6 +14,24 @@ public class HomeController {
 
     @FXML
     private void onBtnLogoutClick() {
-        
+        try {
+            clearSession();
+            LoginView loginView = new LoginView();
+            Stage loginStage = new Stage();
+            loginView.start(loginStage);
+            Stage currentPage = (Stage) btnLogout.getScene().getWindow();
+            currentPage.close();
+            showAlert("Logout", "Anda berhasil logout", Alert.AlertType.INFORMATION);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAlert(String title, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.setHeaderText(null);
+        alert.showAndWait();
     }
 }
