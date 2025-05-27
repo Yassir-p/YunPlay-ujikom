fc5ead74c2e95769754de0717016b2f8c8dae47e
yassir <myassiralhakim@gmail.com>
2025-05-27 08:28:18 +0700

    update

--- src/main/java/com/example/yunplay1/controller/ShowDetailsController.java
+++ src/main/java/com/example/yunplay1/controller/ShowDetailsController.java
@@ -1,7 +1,11 @@
 package com.example.yunplay1.controller;
 
+import com.example.yunplay1.Session;
+import com.example.yunplay1.views.LoginView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ShowDetailsController {
     @FXML
@@ -9,6 +13,24 @@ public class ShowDetailsController {
 
     @FXML
     private void onBtnLogoutClick() {
+        try {
+            Session.clearSession();
+            LoginView loginView = new LoginView();
+            Stage loginStage = new Stage();
+            loginView.start(loginStage);
+            Stage currentPage = (Stage) btnLogout.getScene().getWindow();
+            currentPage.close();
+            showAlert("Logout", "Anda berhasil logout", Alert.AlertType.INFORMATION);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void showAlert(String title, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.setHeaderText(null);
+        alert.showAndWait();
     }
 }
